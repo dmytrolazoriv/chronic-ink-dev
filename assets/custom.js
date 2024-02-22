8480808ef98e73d717b346580aae86a5ee9cd18d
dmytrolazoriv <38793231+dmytrolazoriv@users.noreply.github.com>
2024-02-22 18:14:36 +0200

    Added section, endless-slider (v1), hero-banner (v1)

--- assets/custom.js
+++ assets/custom.js
@@ -10,6 +10,28 @@ document.addEventListener("DOMContentLoaded", () => {
                 hide: false,
                 draggable: true,
               },
+              navigation: {
+                nextEl: ".slider-top__button-next",
+                prevEl: ".slider-top__button-prev",
+            },
+        });
+    }
+
+    if (document.querySelector(".endless-slider__items")) {
+        const endlessSlider = new Swiper('.endless-slider__items', {
+            spaceBetween: 74,
+            grabCursor: false,
+            a11y: false,
+            allowTouchMove: false,
+            freeMode: false,
+            freeModeMomentum: false,
+            speed: 4000,
+            loop: true,
+            slidesPerView: "auto",
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false,
+            },
         });
     }
 });
