919de9fa86a20029a395a77574eded3961f82608
dmytrolazoriv <38793231+dmytrolazoriv@users.noreply.github.com>
2024-02-26 18:13:36 +0200

    Added Testimonials(v1) section and edits for Services(v1.2), Product slider(v1.2), Blog(v1.2)

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,17 @@
+import { ItcCustomSelect } from "./itc-custom-select.js";
+// select-1 – id элемента
+const selectElement = document.querySelector('.itc-select[data-select-index]');
+if (selectElement) {
+    const index = selectElement.getAttribute('data-select-index');
+    if (index) {
+        const select = new ItcCustomSelect(`#select-${index}`);
+    } else {
+        console.error("Data attribute 'data-select-index' not found on select element.");
+    }
+} else {
+    console.error("Select element not found on the page.");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     if (document.querySelector(".slider-main__slider")) {
         const productsSlider = new Swiper(".slider-main__slider", {
